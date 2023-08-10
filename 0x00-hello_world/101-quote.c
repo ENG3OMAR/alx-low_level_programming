071d432150b18915b2afc1ab06800552628e0a96
ENG3OMAR <88eng.omar88@gmail.com>
2023-08-10 23:27:08 +0000

    19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,15 @@
 
 /**
  * main - Entry point
- * Description: this is a c programming which we can't use printf or puts and thier
+ * Description: this is a c programming which we can't 
+ * use printf or puts and thier
  * alternative functions so i'm gonna use write() function
  * from a header file called unistd
  * Return: Always 1 (Success)
  */
 
 	int main(void)
-	{	
-		write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
+	{
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 		return (1);
-	}	
+	}
