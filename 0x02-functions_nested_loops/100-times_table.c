371ac87785c8d22b3f35b1817baa3f0c10cb6073
ENG3OMAR <88eng.omar88@gmail.com>
2023-08-26 21:03:37 +0000

    fixing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,50 @@
 #include "main.h"
 
 /**
- * print_times_table - Function that prints the n times table, starting with 0.
- * @n: an input integer.
- * If n is > than 15 or less than 0 the function should not print anything.
- * Return: nothing.
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
  */
 
 void print_times_table(int n)
 {
-	int i, d, r;
+	int row;
+	int column;
+	int product;
 
 	if (n >= 0 && n <= 15)
 	{
-	for (i = 0; i <= n; ++i)
-	{
-		_putchar('0')
-	for (d = 1; d <= n; ++d)
-	{
-		_putchar(',');
-		_putchar(' ');
-		r = i * d;
-	if (r <= 99)
-		_putchar(' ');
-	if (r <= 9)
-		_putchar(' ');
-	if (r >= 100)
-	{
-		_putchar((r / 100) + '0');
-		_putchar(((r / 10)) % 10 + '0');
-	}
-	else if (r <= 99 && r >= 10)
-	{
-		_putchar((r / 10) + '0');
-	}
-		_putchar((r % 10) + '0');
-	}
+		for (row = 0; row <= n; row++)
+		{
+		for (column = 0; column <= n; column++)
+		{
+			product = (row * column);
+			if (column == 0)
+			_putchar('0' + product);
+			else
+			{
+			_putchar(',');
+			_putchar(' ');
+			if (product <= 9)
+				{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
+			}
+		}
 		_putchar('\n');
-	}
+		}
 	}
 }
